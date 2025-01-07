cb181bab2814852255084f0dfb677d15e9a03668
ON3LNA <lana.maes@student.ucll.be>
2025-01-07 16:53:16 +0100

    img + js.js

---
+++ javascript.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+const identity = { name: 'John', age: 45 }
+console.log(`Hello ${identity.name} who is ${identity.age} years old.`)
+
+
+
+
+
+const identity = { name: 'John', age: 45 }
+const { name, age } = identity
+console.log(`Hello ${name} who is ${age} years old.`)
+
+
+
+
+$
+
+
+
+
+
+
+
+
+
+
+
