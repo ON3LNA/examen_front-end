f762d947b524c7ae328204b1c9f38a148984cf82
ON3LNA <lana.maes@student.ucll.be>
2025-01-12 22:37:02 +0100

    gespeeld met JavaScript

--- dom.js
+++ dom.js
@@ -1,4 +1,5 @@
 const addStatus = (status) =>
     (document.getElementById("status").innerHTML =
       document.getElementById("status").innerHTML + "<p>" + status + "</p>");
-  
+
+console.log(window.innerHeight);
